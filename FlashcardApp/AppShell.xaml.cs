2c3da692193f5d90252e13c4fb5cf9cd0383d166
Jacob Farrow <jacob-farrow@outlook.com>
2026-05-23 13:30:43 -0600

    Scaffold MAUI flashcard project with SQLite packages

---
+++ FlashcardApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FlashcardApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FlashcardApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FlashcardApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
